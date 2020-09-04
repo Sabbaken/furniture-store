f373b217c9b405dc385d1f70586e3ecf5718be00
Kirill Verenich <kirillverenih@ya.ru>
2020-09-04 17:41:00 +0300

    add cloud function for section fetch

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,52 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp(functions.config().firebase);
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+let db = admin.firestore();
+
+exports.fetchSection = functions.https.onRequest((request, response) => {
+
+  let items = [];
+  switch (request.query.section) {
+    case 'livingroom':
+      items = ['tBOAxDwalPgr1CWtGBT8','stNAdaQ3aNdYZqM2WtV2','kXjyw6VKEWkRYWkyvrmy',]
+      break;
+
+    case 'bedroom':
+      items = ['Nse1JjFXLw43BmzgTKhX','stNAdaQ3aNdYZqM2WtV2','WYibtrcVcC6XzYRCu8Z7','',]
+      break;
+
+    case 'kitchen':
+      items = ['WYibtrcVcC6XzYRCu8Z7','IdRtqKcKf08Cv2S4LRlB','stNAdaQ3aNdYZqM2WtV2',]
+      break;
+
+    case 'trending':
+      items = ['5Nkx2TeNxzDrekJqd1lj','IdRtqKcKf08Cv2S4LRlB','Nse1JjFXLw43BmzgTKhX','WYibtrcVcC6XzYRCu8Z7',]
+      break;
+
+    default:
+      items = ['WYibtrcVcC6XzYRCu8Z7','IdRtqKcKf08Cv2S4LRlB','stNAdaQ3aNdYZqM2WtV2',]
+      break;
+  }
+
+  let furnitureRef = db.collection('furniture');
+
+  furnitureRef.get()
+    .then(snapshot => {
+      let selectedItems = [];
+
+      snapshot.forEach(doc => {
+        if (items.indexOf(doc.id) !== -1){
+          selectedItems.push(doc.data())
+        }
+      });
+
+      response.send({section: selectedItems});
+    })
+    .catch(err => {
+      console.log('Error getting documents', err);
+    });
+});
+
+
+
