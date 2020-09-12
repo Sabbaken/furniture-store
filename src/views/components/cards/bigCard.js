c50e5fb96dfd680ed50c6171545e609642099cc3
Kirill Verenich <kirillverenih@ya.ru>
2020-09-12 14:10:36 +0300

    update big card

--- src/views/components/cards/bigCard.js
+++ src/views/components/cards/bigCard.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Carousel from "../carousel/carousel";
+import Stars from "../stars";
 
 const BigCard = ({product}) => {
   return (
@@ -7,13 +8,22 @@ const BigCard = ({product}) => {
       <div>
         <Carousel models={product.models}/>
       </div>
-      <div>
-        stars
-        description
+
+      <div className="big-card__content">
+        <span className="big-card__title">
+          {product.name}
+        </span>
+        <Stars stars={5} value={product.rating - 1}/>
+        <p className="big-card__description">
+          {product.description}
+        </p>
       </div>
-      <div>
-        price
-        btn
+
+      <div className="big-card__content big-card__footer">
+        <span className="big-card__price-tag">
+          ${product.price}
+        </span>
+        <button className="btn btn-primary">cart <img src="/icons/plus.svg" alt="add to cart"/></button>
       </div>
     </div>
   );
